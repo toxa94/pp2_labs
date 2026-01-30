6001307d4f8d686aa29c31a5f681c8b8649ef089
toxa94 <toxagrasso@gmail.com>
2026-01-31 00:00:35 +0500

    Data types

---
+++ W3Schools/5_PythonDataTypes.py
@@ -0,0 +1,12 @@
+# Python Data Types
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
