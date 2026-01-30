954e40c4f712db63da3df8357fb212718f5576c7
toxa94 <toxagrasso@gmail.com>
2026-01-31 00:02:08 +0500

    String

---
+++ W3Schools/8_String.py
@@ -0,0 +1,25 @@
+print("Hello")
+print('Hello')
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+  print(x)
+
+  txt = "The best things in life are free!"
+print("free" in txt)
